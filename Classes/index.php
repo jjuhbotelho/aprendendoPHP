c826bdfe03e52b838e50d11e3f99154a31709060
Julia <juhbotelho.contato@gmail.com>
2020-08-11 19:32:26 -0300

    Adicionando classes

---
+++ Classes/index.php
@@ -0,0 +1,42 @@
+<?php
+
+class Task
+{
+    public $description;
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        //Automaticamente ativado na instanciação.
+
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+}
+
+
+/*A instância de uma classe é um objeto.
+$task = new Task('Go to the store');*/
+
+//Array de tasks
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Watch momma\'s stream'),
+    new Task('Sing with momma'),
+];
+/*Completa a task
+$task->complete();*/
+var_dump($tasks);
+
+$tasks[0]->complete();
+
+require 'index.view.php';
