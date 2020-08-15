1a28edd427343fed54b5a9f1491bbbc9eda85332
Julia <juhbotelho.contato@gmail.com>
2020-08-15 16:51:41 -0300

    Array map, array filter, array column

--- Array/Array Filtering/index.php
+++ Array/Array Filtering/index.php
@@ -5,19 +5,21 @@ class Post
 
     public $title;
     public $published;
+    public $author;
 
-    public function __construct($title, $published)
+    public function __construct($title, $author, $published)
     {
         $this->title = $title;
         $this->published = $published;
+        $this->author = $author;
     }
 }
 
 $posts = [
-    new Post('My First Post', true),
-    new Post('My Second Post', true),
-    new Post('My Third Post', true),
-    new Post('My Fourth Post', false),
+    new Post('My First Post', 'Chico',true),
+    new Post('My Second Post', 'Chumarrento' ,true),
+    new Post('My Third Post', 'Edu Kowalski' ,true),
+    new Post('My Fourth Post', 'Edu Hiromeiro' ,false),
 ];
 
 //$unpublishedPosts = array_filter($posts, function ($post) {
@@ -30,12 +32,27 @@ $posts = [
 //    return $post->published;
 //});
 
-array_map(function ($post) {
 
-    $post->published = true;
+//Duas formas de trazer o título do elemento da Array com array_map
+//$modified = array_map(function ($post) {
+//
+//    return ['tittle' => $post->title];
+//
+//}, $posts);
+//
+//$title = array_map(function($post){
+//    return $post->title;
+//}, $posts);
 
-    return $post;
+//Outra forma de trazer o título usando outro método
+//$titles = array_column($posts, 'title');
 
-}, $posts);
+//foreach ($posts as $index=>$post) {
+//
+//    $posts[$index] = (array) $post;
+//
+//}
 
-var_dump($posts);
+$author = array_column($posts, 'author', 'title');
+
+var_dump($author);
