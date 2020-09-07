20838ac04d16f1d02d4fe9aa13b6339c25fa5c6b
Júlia <juhbotelho.contato@gmail.com>
2020-09-07 19:34:17 -0300

    Criei Model e refatorei o codiguito

--- Laravelson/algumprojeto/app/Http/Controllers/PostController.php
+++ Laravelson/algumprojeto/app/Http/Controllers/PostController.php
@@ -2,17 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\DB;
+use App\Post;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post= DB::table('posts')->where('slug', $slug)->first();
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
