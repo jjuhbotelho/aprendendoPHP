11d48ab75ae4ffa45fec3fef0048576a4374dc0e
Júlia <juhbotelho.contato@gmail.com>
2020-09-14 17:39:03 -0300

    Definindo link ativo

--- Laravelson/algumprojeto/resources/views/layout.blade.php
+++ Laravelson/algumprojeto/resources/views/layout.blade.php
@@ -24,15 +24,15 @@ Released   : 20140225
     <div id="header-wrapper">
         <div id="header" class="container">
             <div id="logo">
-                <h1><a href="#">SimpleWork</a></h1>
+                <h1><a href="/">SimpleWork</a></h1>
             </div>
             <div id="menu">
                 <ul>
-                    <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                    <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                    <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                    <li><a href="#" accesskey="4" title="">Careers</a></li>
-                    <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+                    <li class="{{ Request::path() === '/' ? 'current_page_item' : ''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
+                    <li class="{{ Request::path() === 'clients' ? 'current_page_item' : ''}}"><a href="#" accesskey="2" title="">Our Clients</a></li>
+                    <li class="{{ Request::path() === 'about' ? 'current_page_item' : ''}}"><a href="/about" accesskey="3" title="">About Us</a></li>
+                    <li class="{{ Request::path() === 'careers' ? 'current_page_item' : ''}}"><a href="#" accesskey="4" title="">Careers</a></li>
+                    <li class="{{ Request::path() === 'contact' ? 'current_page_item' : ''}}"><a href="#" accesskey="5" title="">Contact Us</a></li>
                 </ul>
             </div>
         </div>
