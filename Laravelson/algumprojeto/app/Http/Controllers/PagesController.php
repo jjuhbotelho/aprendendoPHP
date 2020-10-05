ded20a09a7ee1afe12f1194fc12cc9c21cc69ab7
Júlia <juhbotelho.contato@gmail.com>
2020-10-05 18:26:24 -0300

    Facade

--- Laravelson/algumprojeto/app/Http/Controllers/PagesController.php
+++ Laravelson/algumprojeto/app/Http/Controllers/PagesController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Example;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Cache;
 
 class PagesController extends Controller
 {
-  public function home(Example $example)
+  public function home()
   {
-    ddd(resolve('App\Example'), resolve('App\Example'));
+    Cache::remember('foo', 60, fn() => 'foobar');
+
+    return Cache::get('foo');
+
+    //    return $file->get(public_path('index.php'));
+    //    return Request::input('name');
+    //    return View::make('welcome');
   }
 }
